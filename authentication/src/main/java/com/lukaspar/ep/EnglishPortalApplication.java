556cad2bd9dd1001b74bbe8b674478e7eb95695e
Lukaspar <lukasz.parsniak@op.pl>
2020-03-24 22:32:44 +0100

    configured submodules and create authentication module

---
+++ authentication/src/main/java/com/lukaspar/ep/EnglishPortalApplication.java
@@ -0,0 +1,13 @@
+package com.lukaspar.ep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishPortalApplication.class, args);
+	}
+
+}
